de82857a0e19f4dbf6e1e505631eb370db37f098
wighawag <wighawag@gmail.com>
2022-05-24 17:15:47 +0100

    remote for common

--- remotes.sh
+++ remotes.sh
@@ -1,2 +1,3 @@
+git remote add conquest-eth-common git@github.com:conquest-eth/conquest-eth-common.git
 git remote add conquest-agent-service git@github.com:conquest-eth/conquest-agent-service.git
 git remote add conquest-account-service git@github.com:conquest-eth/conquest-account-service.git
