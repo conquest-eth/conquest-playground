14ee500ee1a7cf299d8fe90a7ce671daded17834
wighawag <wighawag@gmail.com>
2022-05-24 17:47:33 +0100

    remote for subgraph

--- remotes.sh
+++ remotes.sh
@@ -1,3 +1,4 @@
 git remote add conquest-eth-common git@github.com:conquest-eth/conquest-eth-common.git
+git remote add conquest-eth-subgraph git@github.com:conquest-eth/conquest-eth-subgraph.git
 git remote add conquest-agent-service git@github.com:conquest-eth/conquest-agent-service.git
 git remote add conquest-account-service git@github.com:conquest-eth/conquest-account-service.git
